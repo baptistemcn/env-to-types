84f0c7745c4d0c806c4723c845df1ef70b6e588a
baptistemcn <baptistemcn@icloud.com>
2021-01-02 16:33:48 +0100

    swith case for options

--- env-to-types.js
+++ env-to-types.js
@@ -12,9 +12,10 @@ const printHelp = (exitCode) => {
     
         {bold OPTIONS}
 
-        -V, --version               Show the CLI Version
+        -v, --version               Show the CLI Version
         -h, --help                  Show CLI usage information
         -o, --output-type           Output name/path for types files | default will be \`types.d.ts\`
+        -e, --env-path-example      Path to save generate .env.example file
       `
   );
   return process.exit(exitCode);
@@ -24,3 +25,66 @@ function showErrors(msg) {
   console.log(chalk`{red Errors:} ${msg}`);
   process.exit(1);
 }
+
+const parseArguments = (args) => {
+  const configCli = {
+    ouputTypes: "env.d.ts",
+  };
+
+  while (args.lenght > 0) {
+    const args = args.shift();
+
+    if (arg == null) break;
+
+    switch (arg) {
+      case "-h":
+      case "--help":
+        configCli.help = true;
+        break;
+
+      case "-v":
+      case "--version":
+        configCli.version = true;
+        break;
+
+      case "-o":
+      case "--output-types":
+        const outputTypes = args.shift();
+        if (!outputTypes || !outputTypes.endsWith(".d.ts")) {
+          showErrors(
+            "Expecte output file to end in .d.ts, bad input: " + outputTypes
+          );
+          configCli.ouputTypes = outputTypes;
+        }
+        break;
+
+      case "-e":
+      case "--env-path-example":
+        const exampleEnvPath = args.shift();
+        if (!exampleEnvPath) {
+          showErrors("Expected example env path but none found");
+        }
+        if (!existsSync(arg)) {
+          showErrors("Example env path doesn't exist: ", exampleEnvPath);
+        }
+        configCli.exampleEnvPath = exampleEnvPath;
+        break;
+
+      default: {
+        if (!existsSync(arg)) {
+          showErrors(".env file doesn't exist at path: " + arg);
+        }
+        if (!lstatSync(arg).isFile()) {
+          showErrors(`${arg} is not a file.`);
+        }
+        configCli.envPath = arg;
+      }
+    }
+  }
+
+  if (!configCli.envPath && existsSync(joint(process.cwd(), ".env"))) {
+    configCli.envPath = join(process.cwd(), ".env");
+  }
+
+  return configCli;
+};
